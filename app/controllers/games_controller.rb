6e83231b39fe96130a5d252e7280fd93e901c2d1
Patrick <pdn6991@gmail.com>
2021-08-12 16:05:19 -0400

    refines homepage search

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,10 +1,10 @@
 class GamesController < ApplicationController
   def index
-    random = rand(44000)
+    random = rand(500)
     if params[:search] == ""
-      response = HTTP.headers("Authorization" => "Bearer #{Rails.application.credentials.igdb_key[:access_token]}", "Client-ID" => "#{Rails.application.credentials.igdb_key[:client_id]}").post("https://api.igdb.com/v4/games", :body => "fields id, name, platforms, genres.name, cover.url; where platforms = 6 & themes != [42]; limit 20; offset #{random};")
+      response = HTTP.headers("Authorization" => "Bearer #{Rails.application.credentials.igdb_key[:access_token]}", "Client-ID" => "#{Rails.application.credentials.igdb_key[:client_id]}").post("https://api.igdb.com/v4/games", :body => "fields id, rating, name, platforms, genres.name, cover.url; where platforms = 6 & themes != [42] & cover != null & rating > 70; limit 24; offset #{random};")
     else
-      response = HTTP.headers("Authorization" => "Bearer #{Rails.application.credentials.igdb_key[:access_token]}", "Client-ID" => "#{Rails.application.credentials.igdb_key[:client_id]}").post("https://api.igdb.com/v4/games", :body => 'fields id, name, platforms, genres.name, cover.url; where platforms = (1,6); search "%{game_name}"; limit 50;' % {game_name:params[:search]})
+      response = HTTP.headers("Authorization" => "Bearer #{Rails.application.credentials.igdb_key[:access_token]}", "Client-ID" => "#{Rails.application.credentials.igdb_key[:client_id]}").post("https://api.igdb.com/v4/games", :body => 'fields id, name, platforms, genres.name, cover.url; where platforms = (1,6) & themes != [42] & cover != null; search "%{game_name}"; limit 24;' % {game_name:params[:search]})
     end
 
     render json: response.parse(:json)
