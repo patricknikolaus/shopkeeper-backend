5788ccc79a4e1090c6bffdc3a265e8c818edbca2
Patrick <pdn6991@gmail.com>
2022-01-07 16:41:18 -0500

    final hopefully

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'example.com', 'localhost:8080', 'localhost:4200', 'optimistic-albattani-5f6091.netlify.app'
+    origins 'shopkeepergg.com', 'localhost:8080', 'localhost:4200', 'optimistic-albattani-5f6091.netlify.app'
 
     resource '*',
       headers: :any,
