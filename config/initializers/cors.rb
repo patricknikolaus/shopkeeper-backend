e1bb11a14dd87d9230a39017f8e040c29c76bbe3
Patrick <pdn6991@gmail.com>
2022-01-07 21:36:48 -0500

    blahhh

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'shopkeepergg.netlify.app', 'localhost:8080', 'localhost:4200', 'optimistic-albattani-5f6091.netlify.app'
+    origins 'shopkeepergg.netlify.app', 'localhost:8080', 'localhost:4200', 'shopkeepergg.com'
 
     resource '*',
       headers: :any,
