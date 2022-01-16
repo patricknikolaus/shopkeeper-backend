079898c9782c6e47ee9dcabe11c1c1a1dcc90225
Patrick <pdn6991@gmail.com>
2022-01-16 08:57:52 -0500

    adds url to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'shopkeepergg.netlify.app', 'localhost:8080', 'localhost:4200', 'shopkeepergg.com'
+    origins 'shopkeepergg.netlify.app', 'localhost:8080', 'localhost:4200', 'https://shopkeepergg.com'
 
     resource '*',
       headers: :any,
